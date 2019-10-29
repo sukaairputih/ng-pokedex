419776e8b677b143e4b06febf3e92fba6bf66f2e
sukaairputih <nataniel.susantoputra@outlook.de>
2019-10-29 13:23:59 +0100

    added ng-bootstrap for styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { SharedModule } from './common/shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +19,8 @@ import { AppComponent } from './app.component';
     }),
     HttpClientModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
