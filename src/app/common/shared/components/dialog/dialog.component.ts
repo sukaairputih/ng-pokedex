7afdff00b3713840572a43c5a7df7081d56e2e6d
sukaairputih+ <nataniel.susantoputra@outlook.de>
2020-03-03 13:16:11 +0100

    - added new component - dialog

---
+++ src/app/common/shared/components/dialog/dialog.component.ts
@@ -0,0 +1,47 @@
+import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+// import { Course } from "../model/course";
+import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import * as moment from 'moment';
+
+@Component({
+    selector: 'dialog',
+    templateUrl: './dialog.component.html',
+    styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent implements OnInit {
+
+    form: FormGroup;
+    description:string;
+
+    constructor(
+        private fb: FormBuilder,
+        private dialogRef: MatDialogRef<DialogComponent>,
+        // @Inject(MAT_DIALOG_DATA) {description,longDescription,
+        //     category}:Course 
+        ) {
+
+        // this.description = description;
+
+        // this.form = fb.group({
+        //     description: [description, Validators.required],
+        //     category: [category, Validators.required],
+        //     releasedAt: [moment(), Validators.required],
+        //     longDescription: [longDescription,Validators.required]
+        // });
+
+    }
+
+    ngOnInit() {
+
+    }
+
+    save() {
+        this.dialogRef.close(this.form.value);
+    }
+
+    close() {
+        this.dialogRef.close();
+    }
+
+}
